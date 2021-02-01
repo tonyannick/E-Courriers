faa483a93b2288c2781f8a660f31ae76a6be92f6
DCSI_DEV <stoneyanick@gmail.com>
2021-02-01 13:46:33 +0100

    Ajout du captcha dans la pâge login

--- src/main/java/bean/Login.java
+++ src/main/java/bean/Login.java
@@ -2,15 +2,25 @@ package bean;
 
 import cookieManager.CookiesUtils;
 import databaseManager.UsersQueries;
+import messages.FacesMessages;
 import model.User;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import securityManager.Captcha;
 import sessionManager.SessionUtils;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.imageio.ImageIO;
 import javax.inject.Named;
 import javax.servlet.http.HttpSession;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -21,6 +31,9 @@ public class Login implements Serializable {
     private static final long serialVersionUID = -486667257950195964L;
     private User user;
     private String userSessionUniqueId;
+    private StreamedContent imageCaptcha;
+    private String valeurCaptcha;
+    private String captcha;
 
     @PostConstruct
     public void initialisation(){
@@ -28,40 +41,81 @@ public class Login implements Serializable {
         user.setSeSouvenir(false);
     }
 
-    /**Methode de clic sur le bouton valider**/
-    public String cliquerSurValider(){
-        if(UsersQueries.verifierUserLogin(user.getUserlogin(),user.getUserPassword())){
-            userSessionUniqueId = UUID.randomUUID().toString();
-            HttpSession session = SessionUtils.getSession();
-            session.setAttribute("uniqueUserID",userSessionUniqueId);
-            session.setAttribute("userName", UsersQueries.nomCompletUser);
-            session.setAttribute("idUser",UsersQueries.idPersonne);
-            session.setAttribute("id_etablissement",UsersQueries.idEtablissement);
-            session.setAttribute("directionUser", UsersQueries.directionUser);
-            session.setAttribute("fonctionUser", UsersQueries.fonctionUser);
-            session.setAttribute("profilUser", UsersQueries.profilUser);
-            session.setAttribute("premiereConnexion", "oui");
-            session.setAttribute("idDirectionUser", UsersQueries.idDirectionUser);
-            if(user.isSeSouvenir()) {
-                CookiesUtils.creerUnCookie("cookieIdentifiant",user.getUserlogin());
-                CookiesUtils.creerUnCookie("cookieMotDePasse",user.getUserPassword());
-                CookiesUtils.creerUnCookie("seSouvenirDeMoi","oui");
-            }
-            UsersQueries.recupererInfosDeSession();
-            session.setAttribute("isResponsable", UsersQueries.isResponsable);
-            session.setAttribute("isSecretaire", UsersQueries.isSecretaire);
+    public void actualiserImageCaptcha(){
 
-            return "tableaudebord?faces-redirect=true";
-        }else{
-            FacesContext context = FacesContext.getCurrentInstance();
-            context.getExternalContext().getFlash().setKeepMessages(true);
-            FacesContext.getCurrentInstance().addMessage("messagesLogin", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erreur!!!",
-                    "Identifiant ou mot de passe invalide"));
-            return "login?faces-redirect=true";
-        }
+        captcha = Captcha.captchaGenerator(9);
+        imageCaptcha = DefaultStreamedContent.builder().contentType("image/png").stream(() -> {
+
+            int width = 140, height = 30;
+            BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.OPAQUE);
+            Graphics graphics = bufferedImage.createGraphics();
+            graphics.setFont(new Font("Arial", Font.BOLD, 20));
+            graphics.setColor(new Color(169, 169, 169));
+            graphics.fillRect(0, 0, width, height);
+            graphics.setColor(new Color(255, 255, 255));
+            graphics.drawString(captcha, 20, 25);
+
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            try {
+                ImageIO.write(bufferedImage, "png", os);
+                return new ByteArrayInputStream(os.toByteArray());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }).build();
 
     }
 
+    /**Methode de clic sur le bouton valider**/
+    public String cliquerSurValider(){
+
+        if(verifierCaptcha()){
+            System.out.println("je suis ici");
+            if(UsersQueries.verifierUserLogin(user.getUserlogin(),user.getUserPassword())){
+                userSessionUniqueId = UUID.randomUUID().toString();
+                HttpSession session = SessionUtils.getSession();
+                session.setAttribute("uniqueUserID",userSessionUniqueId);
+                session.setAttribute("userName", UsersQueries.nomCompletUser);
+                session.setAttribute("idUser",UsersQueries.idPersonne);
+                session.setAttribute("id_etablissement",UsersQueries.idEtablissement);
+                session.setAttribute("directionUser", UsersQueries.directionUser);
+                session.setAttribute("fonctionUser", UsersQueries.fonctionUser);
+                session.setAttribute("profilUser", UsersQueries.profilUser);
+                session.setAttribute("premiereConnexion", "oui");
+                session.setAttribute("idDirectionUser", UsersQueries.idDirectionUser);
+                if(user.isSeSouvenir()) {
+                    CookiesUtils.creerUnCookie("cookieIdentifiant",user.getUserlogin());
+                    CookiesUtils.creerUnCookie("cookieMotDePasse",user.getUserPassword());
+                    CookiesUtils.creerUnCookie("seSouvenirDeMoi","oui");
+                }
+                UsersQueries.recupererInfosDeSession();
+                session.setAttribute("isResponsable", UsersQueries.isResponsable);
+                session.setAttribute("isSecretaire", UsersQueries.isSecretaire);
+
+                return "tableaudebord?faces-redirect=true";
+            }else{
+                FacesContext context = FacesContext.getCurrentInstance();
+                context.getExternalContext().getFlash().setKeepMessages(true);
+                FacesMessages.errorMessage("messagesLogin","Erreur!!!","Identifiant ou mot de passe invalide");
+                return "login?faces-redirect=true";
+            }
+        }else{
+            System.out.println("je suis la");
+            FacesMessages.errorMessage("messagesLogin","Erreur!!!","Le texte du captcha n'est pas correcte");
+            return "login?faces-redirect=true";
+        }
+    }
+
+    private boolean verifierCaptcha(){
+        boolean isOk = false;
+        if(valeurCaptcha.equals(captcha)){
+            isOk = true;
+        }
+        System.out.println("isOk = " + isOk);
+        return isOk;
+    }
 
     public String clicSurVersMotDePasseOublie(){
         return "motdepasseoublie?faces-redirect=true";
@@ -94,5 +148,21 @@ public class Login implements Serializable {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public StreamedContent getImageCaptcha() {
+        return imageCaptcha;
+    }
+
+    public void setImageCaptcha(StreamedContent imageCaptcha) {
+        this.imageCaptcha = imageCaptcha;
+    }
+
+    public String getValeurCaptcha() {
+        return valeurCaptcha;
+    }
+
+    public void setValeurCaptcha(String valeurCaptcha) {
+        this.valeurCaptcha = valeurCaptcha;
     }
 }
